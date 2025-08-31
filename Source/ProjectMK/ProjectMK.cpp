260d8c602e13042cfbcd4ea1f3abd0b5ab85fc28
KimHanWol <linkingmain@gmail.com>
2025-08-31 23:09:09 +0900

    Project 생성 및 에셋 테스트

---
+++ Source/ProjectMK/ProjectMK.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectMK.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectMK, "ProjectMK" );
