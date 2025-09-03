0ce8876878665e1e014a397f5b293138d724d982
KimHanWol <linkingmain@gmail.com>
2025-09-03 22:50:50 +0900

    레벨 및 캐릭터 기본 세팅 추가 - 캐릭터 기본 움직임 추가 - 콜리전 세팅

---
+++ Source/ProjectMK/Actor/Character/MKCharacter.cpp
@@ -0,0 +1,40 @@
+// LINK
+
+#include "ProjectMK/Actor/Character/MKCharacter.h"
+
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "PaperSpriteComponent.h"
+
+AMKCharacter::AMKCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	SetRootComponent(GetCapsuleComponent());
+
+	SpriteComponent = CreateDefaultSubobject<UPaperSpriteComponent>(TEXT("PaperSpriteComponent"));
+	SpriteComponent->SetupAttachment(RootComponent);
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetupAttachment(SpriteComponent);
+}
+
+void AMKCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AMKCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMKCharacter::MoveRight);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AMKCharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &AMKCharacter::StopJumping);
+}
+
+void AMKCharacter::MoveRight(float Value)
+{
+	AddMovementInput(FVector::ForwardVector, Value * MoveSpeed);
+}
+
---
+++ Source/ProjectMK/Actor/Character/MKCharacter.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MKCharacter.generated.h"
+
+class UPaperSpriteComponent;
+class UCameraComponent;
+
+UCLASS()
+class PROJECTMK_API AMKCharacter : public ACharacter
+{
+	GENERATED_BODY()
+	
+public:	
+	AMKCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+private:
+	void MoveRight(float Value);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UPaperSpriteComponent> SpriteComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UCameraComponent> CameraComponent;
+
+	UPROPERTY(EditAnywhere)
+	float MoveSpeed = 1.f;
+};
